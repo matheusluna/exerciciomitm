43577b13db29c25c4c469a87e4f55bc1f3f62f19
matheusluna <matheusluna96@gmail.com>
2018-08-28 07:31:37 -0300

    adicionando código

---
+++ src/main/java/br/edu/ifpb/servlets/Main.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author mathe
+ */
+@WebServlet(name = "Main", urlPatterns = {"/Main"})
+public class Main extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        String user = request.getParameter("username");
+        String password = request.getParameter("password");
+        String driver = "org.postgresql.Driver";
+        String usersql = "postgres";
+        String senha = "123456";
+        String url = "jdbc:postgresql://localhost:5432/seguranca";
+        try {
+            Class.forName(driver);
+            Connection con = null;
+            con = DriverManager.getConnection(url, usersql, senha);
+            String sql = "insert into usuario values (?,?)";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, user);
+            stmt.setString(2, password);
+            stmt.execute();
+            con.close();
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.err.print(ex.getMessage());
+        }
+        response.sendRedirect("https://suap.ifpb.edu.br/accounts/login/?next=/");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
